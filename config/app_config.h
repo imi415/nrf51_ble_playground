10f33b2408675a463a0aabe99ad2e57a8edf3c6f
imi415 <imi415.public@gmail.com>
2021-11-25 03:02:27 +0800

    Applied upstream fixes, added BLE template.

--- config/app_config.h
+++ config/app_config.h
@@ -2,5 +2,31 @@
 #define APP_CONFIG_H
 
 /* Define your App config here */
-#define NRF_LOG_ENABLED 1
+
+/* BLE config */
+#define BLE_ADVERTISING_ENABLED 1
+#define PEER_MANAGER_ENABLED    1
+
+/* GPIOTE config */
+#define GPIOTE_ENABLED 1
+
+/* Button config */
+#define BUTTON_ENABLED 1
+
+/* CRC16 config */
+#define CRC16_ENABLED 1
+
+/* Flash Data Storage (FDS) config */
+#define FDS_ENABLED 1
+
+/* Flash Storage config */
+#define FSTORAGE_ENABLED 1
+
+/* Logging config */
+#define NRF_LOG_ENABLED       1
+#define NRF_LOG_USES_COLORS   1
+#define NRF_LOG_COLOR_DEFAULT 3
+#define NRF_LOG_WARNING_COLOR 4
+#define NRF_LOG_ERROR_COLOR   2
+
 #endif
